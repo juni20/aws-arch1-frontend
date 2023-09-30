1978ef8284fba219166d261b8bf15a0977f4c342
juni20 <juni80105@gmail.com>
2023-09-30 19:08:04 +0900

    modified:   src/components/HeaderComponent.jsx

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,8 +12,9 @@ export default class HeaderComponent extends Component {
         <header>
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
-              <a href="http://www.fastcampus-dragon.net" className="navbar-brand">
-                kimdragon - jenkins
+              {/* <a href="http://www.fastcampus-dragon.net" className="navbar-brand"> */}
+              <a href="http://www.juni105.net" className="navbar-brand">
+                junheelee - jenkins
               </a>
             </div>
           </nav>
